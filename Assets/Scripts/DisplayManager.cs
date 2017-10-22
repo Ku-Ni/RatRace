95ba01c257d3c2008b411e2276180fa4bb858005
Coenie <coenie.basson@gmail.com>
2017-10-22 21:39:31 +0100

    Re-adding all

---
+++ Assets/Scripts/DisplayManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using System;
+
+public class DisplayManager : MonoBehaviour {
+    private List<GameObject> displayObjects;
+    private GameObject storeDisplay;
+    private GameObject workDisplay;
+
+    // Use this for initialization
+    void Start() {
+        displayObjects = new List<GameObject>(GameObject.FindGameObjectsWithTag("display"));
+        foreach (GameObject displayObject in displayObjects) {
+            displayObject.SetActive(false);
+
+            if (displayObject.name == "StoreDisplay")
+                storeDisplay = displayObject;
+            else if (displayObject.name == "WorkDisplay")
+                workDisplay = displayObject;
+        }
+        
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public void Deactivate() {
+        foreach(GameObject display in displayObjects){
+            display.SetActive(false);
+        }        
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="store"></param>
+    public void DisplayStore(Store store) {
+        Text text = storeDisplay.GetComponentInChildren<Text>();
+        List<StoreItem> storeItems = store.GetItems();
+
+        foreach (StoreItem item in storeItems) {
+            text.text += item.ToString();
+            text.text += Environment.NewLine;
+        }
+
+        storeDisplay.SetActive(true);
+
+    }
+}
---
+++ Assets/Scripts/StoreManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+using UnityEngine;
+
+public class StoreManager : MonoBehaviour {
+    private StoreItemsContainer storeItemsContainer;
+    public string storeItemsPath = "data/StoreItems.xml";
+
+    // Use this for initialization
+    void Start () {
+        Debug.Log("StoreManager initialising...");
+        storeItemsContainer = LoadStoreItemsContainer();
+        Debug.Log("StoreItemsContainer: " + storeItemsContainer.ToString());
+    }
+	
+
+    private StoreItemsContainer LoadStoreItemsContainer() {
+        Debug.Log("Datapath: " + System.IO.Path.Combine(Application.dataPath, storeItemsPath));
+        var serializer = new XmlSerializer(typeof(StoreItemsContainer));
+
+        using (var stream = new FileStream(System.IO.Path.Combine(Application.dataPath, storeItemsPath), FileMode.Open)) {
+            return serializer.Deserialize(stream) as StoreItemsContainer;
+        }
+    }
+
+    public List<StoreItem> GetStoreItems(string storeName) {
+        Debug.Log("StoreName: " + storeName);
+        if (storeItemsContainer == null)
+        {
+            storeItemsContainer = LoadStoreItemsContainer();
+        }
+        Debug.Log("StoreItemsContainer: " + storeItemsContainer.ToString());
+        return storeItemsContainer.GetStoreItems(storeName);
+    }
+}
